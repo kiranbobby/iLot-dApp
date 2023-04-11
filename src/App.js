e159853d310ff971c1a1966a755234ca481fb5dc
kiranrevadi <kiranr@ideyalabs.com>
2023-04-11 07:18:52 +0530

    Chages for landing View

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/properties" element={<Properties />} />
